f458ee9ba1cde642847d262da6ca6de1dccc8594
Vojin Jovanovic <vojin.jovanovic@epfl.ch>
2013-01-14 13:50:10 +0100

    Adding configuration of macros and debugging.

--- src/test/scala/PrototypeSpec.scala
+++ src/test/scala/PrototypeSpec.scala
@@ -13,7 +13,7 @@ class PrototypeSpec extends FlatSpec with ShouldMatchers {
   def paramMethod(i: Int) = i
   
   "A shallow embedding of la" should "produce values" in {
-     val x = la {
+     val x = /*la*/ {
        val v1 = DenseVector(1,2,3)
        (v1 + (DenseVector(3,4,5) * SparseVector[Int](6,7,8)): Vector[Int]).map(_ + 1)
      }
@@ -21,42 +21,30 @@ class PrototypeSpec extends FlatSpec with ShouldMatchers {
      x should equal (DenseVector(20, 31, 44))
   }
 
-  /*it should "compile with just literal" in {
-    val x = laLifted("test", 1 + 2)
-  }*/
-  
-  it should "compile with captured values" in {
+  it should "lift only fields, functions, and variables that are not bound in the DSL scope" in {
     def y(i: Int) = {1}
     def function = 1
     val (t1, t2) = (1,2)
-
-    // TODO decide what should be allowed
-    // Thoughts: 
-    //   outer vars and vals should be accepted  
-    //   fields? Why not? This will not create a confusion.
-    //   methods? Why not? This could not create a confusion since parameters of methods can be arbitrary Scala. Explicitly mark it or not?
-    // Conclusion - for the time being we can lift all identifiers freely. Later we will fine tune the transformer for fields and methods.
     
-    val dslResult = laLifted("test", {
+    val dslResult = la {
+//      This does not work for now!!      
 //      pfield
 //      field
 //      vfield
 //      method      
 //      y(1) // we can leave this out for now
-//      function
       def m = 1
       val in = 1 
       in
       m 
       function
       t1
-    })
+    }
   }
   
-  // let's do baby steps first
-  it should "work with a simple object" in {
-    //val x = laLifted { DenseVector(1, 2) }
-    //x shouldBe la.DenseVector(1,2)
+  it should "compile method application on an object" in {
+     val x = /*laDebug*/ { DenseVector(1, 2) }
+     x shouldBe DenseVector(1,2)
   }
   
 }
